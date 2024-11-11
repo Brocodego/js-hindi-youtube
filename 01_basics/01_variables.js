f3dbb5524440b3bd713c42a47c883f527864758a
Brocodego <153532797+Brocodego@users.noreply.github.com>
2024-11-11 16:42:41 +0000

    learned variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 144553
+let accountEmail="exactcoding@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+let accountState;
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
