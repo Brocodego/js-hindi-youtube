5231e333f25976bcda23795337b0bea2a809b121
Brocodego <153532797+Brocodego@users.noreply.github.com>
2024-11-12 04:12:36 +0000

    datatypes learned

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+ let name="aniket"
+ let age=18
+ let isLoggedIn= false
+
+ //number
+ //bigint
+ //String
+ //boolean
+ //null =>standalone value
+ //undefined
+ //symbol
+
+ //object
+ console.log(typeof "aniket");
+ console.log(age);
+ console.log(null);
+ console.log(undefined);
+ 
+ 
+ 
+ 
