b7c8f8124cc45103de7f624dc3fd318b53239b27
Brocodego <153532797+Brocodego@users.noreply.github.com>
2024-11-14 16:59:56 +0000

    know about stack and heap memory

---
+++ 01_basics/07_stackandheapmemory.js
@@ -0,0 +1,24 @@
+//stack(Primitive),Heap(Non-Primitive)
+// let myYoutubename="hiteshchoudhary";
+// let anothername=myYoutubename;
+// console.log(myYoutubename);
+// console.log(anothername);
+// anothername="chaiaurcode"
+// console.log(myYoutubename);
+// console.log(anothername);
+
+let userOne={
+    email:"user@google.com",
+    upi:"user@ybl"
+}
+console.log(userOne);
+
+let userTwo=userOne;
+userTwo.email="aniket@google.com"
+console.log(userTwo);
+console.log(userTwo);
+
+
+
+
+
