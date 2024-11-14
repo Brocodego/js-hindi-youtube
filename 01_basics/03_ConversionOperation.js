8ce08050555feeafe3ab7e29099fe03e9e81a6af
Brocodego <153532797+Brocodego@users.noreply.github.com>
2024-11-14 15:51:54 +0000

    learned conversion

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,15 @@
+let score ="123abc"
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+//"33" =>33
+//"33abc"=>NaN
+//true = 1; false
+
+let number =33
+let newstring = String(number)
+console.log(typeof newstring)
+console.log(newstring);
+
+
+
