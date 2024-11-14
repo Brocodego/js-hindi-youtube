c8a1faf6d91bdf4b237f1f6b8518cdf71abb5196
Brocodego <153532797+Brocodego@users.noreply.github.com>
2024-11-14 16:39:22 +0000

    Datatypes Summary

---
+++ 01_basics/06_datatypes summary.js
@@ -0,0 +1,41 @@
+//Primitive Datatypes
+//7 types: String,Number,,Boolean,null(empty),undefined,symbol,BigInt
+//Refence(Non-Primitive)
+//Array,Objects,Functions
+
+const score=100;
+const scoreValue=100.3;
+const isLoggedIN=false;
+const outsideTemp=null;
+let userEmail;
+
+// const id=Symbol('123');
+// const anotherId=Symbol('123');
+
+// console.log(id===anotherId);
+const bigNumber=234567898765;
+
+// ****************Non primitive types*****************************
+const heros=["shaktiman","naagraj","doga"]
+let myobj={
+    name:"aniket",
+    age:22
+}
+
+// console.log(heros);
+// console.log(myobj);
+
+const myFunction =function(){
+    console.log("Hello World"); 
+}
+
+console.log(typeof bigNumber);
+console.log(typeof outsideTemp);
+console.log(typeof myFunction);
+
+console.log(typeof myobj);
+
+
+
+
+
