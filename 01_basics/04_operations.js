82869960e50d8dd8b67bf155574ac8c57783ab26
Brocodego <153532797+Brocodego@users.noreply.github.com>
2024-11-14 15:52:05 +0000

    learned operations

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,26 @@
+// let value=3;
+// let negvalue=-(value)
+// console.log(negvalue);
+// let str1="hello"
+// let str2=" aniket"
+// let str3=str1+str2;
+// console.log(str3);
+
+// console.log((3+4)*5%3);
+console.log(true);
+console.log(+true);
+// console.log(-true);
+
+let num1,num2,num3;
+num1=num2=num3=2+2;
+console.log(num1);
+
+let gameCounter=100;
+++gameCounter;
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
