846f40a8d191ff4923a961996fe625f34e876087
Brocodego <153532797+Brocodego@users.noreply.github.com>
2024-11-15 05:19:04 +0000

    revise strings constantly

---
+++ 01_basics/08_strings.js
@@ -0,0 +1,36 @@
+// const name ="Aniket"
+// const repoCount=4;
+// console.log(`Hello my name is ${name} and my repo Count is ${repoCount}`);
+const gameName=new String('Aniketbro') //String Declearation
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('i'));
+
+// const newString=gameName.substring(0,5);
+// console.log(newString);
+// const anotherString=gameName.slice(-7,4);
+// console.log(anotherString);
+// const newStringOne="              Guddu pandit      "
+// console.log(newStringOne.trim());
+const url="https://aniket.com/aniket%20rathour"
+console.log(url.replace('%20','-'));
+console.log(url.includes('hitesh'));
+console.log(url.includes('aniket'));
+const broname="aniket-bro-rathour"
+console.log(broname.split('-'));
+
+
+
+
+
+
+
+
+
+
+
+
