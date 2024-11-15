5098dba51c39c7ce0e0e5aecd69b277a9732a90d
Brocodego <153532797+Brocodego@users.noreply.github.com>
2024-11-15 07:53:54 +0000

    number and maths learned

---
+++ 01_basics/09_numbersandmaths.js
@@ -0,0 +1,49 @@
+// const score=400;
+// console.log(score);
+const balance=new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(4));
+
+// const otherNumber=23.8966
+// const otherNumberOne=123.8998
+// console.log(otherNumber.toPrecision(4));
+// console.log(otherNumberOne.toPrecision(3));
+
+// const hundreds=1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// *************************Maths**********************
+// console.log(Math.abs(-5));
+// console.log(Math.round(4.3));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(3,4,8,9));
+// console.log(Math.max(3,4,8,9));
+
+// console.log(Math.random());
+// console.log(Math.random()*10);
+// console.log((Math.random()*10)+1);
+
+const min=10
+const max=20
+console.log(Math.random()*(max-min+1));
+console.log(Math.random()*(max-min+1)+min);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
